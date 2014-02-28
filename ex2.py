4905484669b9288c375717c500f814d128853c71
lusma11 <lulusmall@gmail.com>
2014-02-28 14:18:14 +0000

    Just a hash tag
    Hash tag before print

--- ex2.py
+++ ex2.py
@@ -6,5 +6,6 @@ print "I could have code like this." # and the comment after is ignored
 # You can also a comment to "disable" or comment out a piece of code:
 # print "This won't run."
 
-print "This will run."
+#print "This will run."
+
 
